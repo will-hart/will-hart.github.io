e3af82e25e310d2312f6394c1929eadc2a616aaa
William Hart <hart.wl@gmail.com>
2022-01-20 22:11:00 +1100

    Add passHref prop

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -10,7 +10,7 @@ interface PostListProps {
 const PostListItem = ({ post }: { post: BlogPostSummary }): JSX.Element => (
   <div className="flex flex-col w-full lg:w-1/3 xl:w-1/4 m-4 box-border bg-gray-700">
     <div className="flex flex-col p-4">
-      <Link href={`/post/${post.slug}`}>
+      <Link href={`/post/${post.slug}`} passHref>
         <a className="text-lg font-bold text-white">
           <h2 className="text-lg font-bold lg:h-16">{post.title}</h2>
         </a>
@@ -18,7 +18,7 @@ const PostListItem = ({ post }: { post: BlogPostSummary }): JSX.Element => (
       <p className="text-md text-gray-400 lg:h-24 my-2">{post.summary}</p>
     </div>
 
-    <Link href={`/post/${post.slug}`}>
+    <Link href={`/post/${post.slug}`} passHref>
       <div className="hero hover:bg-yellow-300 border-t-2 border-yellow-300 cursor-pointer text-yellow-300 hover:text-darkbg p-2 flex justify-between transition-colors duration-500">
         <a className="text-current no-underline hover:no-underline">
           Read More
