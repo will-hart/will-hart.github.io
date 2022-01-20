3097e3fe11f8dc1d5aee51e714daeea8946b2151
William Hart <hart.wl@gmail.com>
2022-01-20 23:50:44 +1100

    Use optimised next images if w/h set in alt

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -8,6 +8,8 @@ import { CodeProps } from "react-markdown/lib/ast-to-react"
 import syntaxTheme from "./syntaxTheme"
 
 import "katex/dist/katex.min.css"
+import Image from "next/image"
+import { getShimmerDataUrl } from "../utilities/blurPlaceholder"
 
 const customRenderers = {
   code: function Code({
@@ -32,6 +34,40 @@ const customRenderers = {
       <code className={className} {...props}>
         {children}
       </code>
+    )
+  },
+  img: function NextImage({
+    src,
+    alt,
+  }: React.DetailedHTMLProps<
+    React.ImgHTMLAttributes<HTMLImageElement>,
+    HTMLImageElement
+  >): JSX.Element {
+    // images should be marked with e.g. `(size: 190x30px)` for loading to be optimised
+    const re = /\(size: ([0-9]+)x([0-9]+)px\)/
+    const result = (alt || "").match(re)
+
+    if (!result) {
+      // I may as well use an image tag here because I don't know anything about the image and therefore
+      // next's fancy optimisation doesn't work. Ideally next would realise that the images are dynamically
+      // loaded but local, but that doesn't seem to be possible yet.
+      // eslint-disable-next-line @next/next/no-img-element
+      return <img src={src} alt={alt} />
+    }
+
+    const width = parseInt(result[1], 10)
+    const height = parseInt(result[2], 10)
+
+    return (
+      <Image
+        className="optimisedImage"
+        src={src}
+        width={width}
+        height={height}
+        blurDataURL={getShimmerDataUrl(width, height)}
+        alt={alt}
+        layout="responsive"
+      />
     )
   },
 }
